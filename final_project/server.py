c1d5eccb5dc349b4848926b243b6a4279fd10a90
JovenSanty <javiber762@gmail.com>
2026-03-23 17:38:47 -0400

    exito

--- final_project/server.py
+++ final_project/server.py
@@ -19,5 +19,5 @@ def emotionDetector():
     )
     return formatted_response
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5500)
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=5500)
